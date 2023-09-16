582526857e658ebf6e4454e5a7a08b30c0b1eda0
Oleg Bilas <olegbilas@gmail.com>
2023-09-16 20:59:03 +0300

    Update getInfoAboutCar.js

--- src/utils/getInfoAboutCar.js
+++ src/utils/getInfoAboutCar.js
@@ -22,23 +22,18 @@ export const getSecondLine = ({ type, model, mileage, functionalities }) => {
   return [type, model, mileage, functionalities[0]];
 };
 
-export const getFirstLineModal = ({ address, rentalCompany, accessories }) => {
+export const getFirstLineModal = ({
+  address,
+  rentalCompany,
+  id,
+  year,
+  type,
+}) => {
   const { city, country } = getCarLocation(address);
 
-  const array = [city, country, rentalCompany];
-
-  for (const item of accessories) {
-    if (item.toLowerCase().contains('premium')) return array.push('Premium');
-  }
-
-  return array;
+  return [city, country, `id: ${id}`, `Year: ${year}`, `Type: ${type}`];
 };
 
-export const getSecondLineModal = ({
-  type,
-  model,
-  mileage,
-  functionalities,
-}) => {
-  return [type, model, mileage, functionalities[0]];
+export const getSecondLineModal = ({ fuelConsumption, engineSize }) => {
+  return [`Fuel Consumption: ${fuelConsumption}`, `Engine Size: ${engineSize}`];
 };
